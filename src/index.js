8d41f6e058c0079d6daf01e165979b140e02ee5f
Siddhant Bagga <siddhantbagga@UCLP1905.local>
2019-07-23 16:26:21 +0530

    init commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+
+const multer = require('multer')
+const upload = multer({
+  dest: 'images',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      er = new Error('Please upload a Word document');
+      return cb(new Error('Please upload a Word document'))
+      
+    }
+
+    cb(undefined, true)
+  }
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+  res.send()
+})
+
+
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+  console.log('Server is up on port ' + port)
+})
